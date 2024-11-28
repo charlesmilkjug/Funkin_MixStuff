cb87a4c6201e7dc9aae7debf4e583cdbed9d584b
amyspark-ng ✨ <92493175+amyspark-ng@users.noreply.github.com>
2024-11-27 23:53:36 -0500

    removed dumb comment

--- source/funkin/ui/freeplay/FreeplayState.hx
+++ source/funkin/ui/freeplay/FreeplayState.hx
@@ -526,8 +526,7 @@ class FreeplayState extends MusicBeatSubState
       {
         FunkinSound.playOnce(Paths.sound('scrollMenu'), 0.4);
 
-        // stuff here
-
+        // This makes it so it sends you to the same song in the new list (if it's there).
         if (grpCapsules.members[curSelected].freeplayData != null)
         {
           curSelected = grpCapsules.members.indexOf(grpCapsules.members[curSelected]);
